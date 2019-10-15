db98778a638d28958fd904254b43dc4a0e30bd0e
william <fenglm999@126.com>
2019-10-15 09:24:57 +0800

    di

---
+++ MathDraw/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MathDraw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtXmin = new System.Windows.Forms.TextBox();
+            this.txtYmin = new System.Windows.Forms.TextBox();
+            this.txtXmax = new System.Windows.Forms.TextBox();
+            this.txtYmax = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGraph = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(88, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(612, 375);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtXmin
+            // 
+            this.txtXmin.Location = new System.Drawing.Point(88, 13);
+            this.txtXmin.Name = "txtXmin";
+            this.txtXmin.Size = new System.Drawing.Size(87, 21);
+            this.txtXmin.TabIndex = 1;
+            this.txtXmin.Text = "-1.1";
+            // 
+            // txtYmin
+            // 
+            this.txtYmin.Location = new System.Drawing.Point(253, 13);
+            this.txtYmin.Name = "txtYmin";
+            this.txtYmin.Size = new System.Drawing.Size(100, 21);
+            this.txtYmin.TabIndex = 2;
+            this.txtYmin.Text = "-2.1";
+            // 
+            // txtXmax
+            // 
+            this.txtXmax.Location = new System.Drawing.Point(88, 62);
+            this.txtXmax.Name = "txtXmax";
+            this.txtXmax.Size = new System.Drawing.Size(87, 21);
+            this.txtXmax.TabIndex = 3;
+            this.txtXmax.Text = "3.1";
+            // 
+            // txtYmax
+            // 
+            this.txtYmax.Location = new System.Drawing.Point(253, 61);
+            this.txtYmax.Name = "txtYmax";
+            this.txtYmax.Size = new System.Drawing.Size(100, 21);
+            this.txtYmax.TabIndex = 4;
+            this.txtYmax.Text = "2.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X Min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X Max";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Y Min";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Y Max";
+            // 
+            // btnGraph
+            // 
+            this.btnGraph.Location = new System.Drawing.Point(403, 10);
+            this.btnGraph.Name = "btnGraph";
+            this.btnGraph.Size = new System.Drawing.Size(75, 23);
+            this.btnGraph.TabIndex = 9;
+            this.btnGraph.Text = "Graph";
+            this.btnGraph.UseVisualStyleBackColor = true;
+            this.btnGraph.Click += new System.EventHandler(this.btnGraph_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 487);
+            this.Controls.Add(this.btnGraph);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtYmax);
+            this.Controls.Add(this.txtXmax);
+            this.Controls.Add(this.txtYmin);
+            this.Controls.Add(this.txtXmin);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtXmin;
+        private System.Windows.Forms.TextBox txtYmin;
+        private System.Windows.Forms.TextBox txtXmax;
+        private System.Windows.Forms.TextBox txtYmax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGraph;
+    }
+}
+
